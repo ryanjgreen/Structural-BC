1c1488620f8feee92abad4906e3ac93830d06a63
Ryan Green <greenryanhockey@gmail.com>
2023-08-25 11:49:11 -0700

    yeeeee

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -19,7 +19,7 @@ export default function Form() {
             }
     
             const { firstName, lastName, email, password } = formData; // Destructure the fields
-            const response = await axios.post('https://structuralbc-956ac776b3e6.herokuapp.com/register', {
+            const response = await axios.post('https://structuralbc-cea735a41380.herokuapp.com//register', {
                 firstName,
                 lastName,
                 email,
